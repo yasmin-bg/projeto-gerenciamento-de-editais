e1a1390e86f244531463d93c9f7e3d1715b8fa35
unknown <carina.flor04@gmail.com>
2024-11-23 15:28:00 -0300

    Adicionando dao da disciplina e melhorias no dto

--- ProjetoBancoDeDados/src/main/java/dao/DisciplinaDAO.java
+++ ProjetoBancoDeDados/src/main/java/dao/DisciplinaDAO.java
@@ -1,14 +1,11 @@
 package dao;
 
-import java.util.List;
-
 import dto.DisciplinaDTO;
 
 public interface DisciplinaDAO {
 	
-	public DisciplinaDTO salvar(DisciplinaDTO dto);
+	public void salvar(DisciplinaDTO dto);
 	public DisciplinaDTO buscarPorId(DisciplinaDTO dto);
-	public List<DisciplinaDTO> listarTodos(DisciplinaDTO dto);
 	public void excluir(DisciplinaDTO dto);
 
 }
